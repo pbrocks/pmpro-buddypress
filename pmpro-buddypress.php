9f6671d92eb0250868de43dd720a5e7d8901069b
Paul Barthmaier <paul@barthmaier.rocks>
2018-09-16 22:59:59 -0400

    changing version to 1.3

--- pmpro-buddypress.php
+++ pmpro-buddypress.php
@@ -3,14 +3,14 @@
  Plugin Name: Paid Memberships Pro - BuddyPress Add On
  Plugin URI: https://www.paidmembershipspro.com/add-ons/buddypress-integration
  Description: Manage access to your BuddyPress Community using Paid Memberships Pro.
- Version: 1.2
+ Version: 1.3
  Author: Paid Memberships Pro
  Author URI: https://www.paidmembershipspro.com
  Text Domain: pmpro-buddypress
  */
 
 /*
-    includes
+	includes
 */
 define( 'PMPROBP_DIR', dirname( __FILE__ ) );
 define( 'PMPROBP_BASENAME', plugin_basename( __FILE__ ) );
